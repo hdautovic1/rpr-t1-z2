2d38fb704e9d2e2185bd62cc6c1bdffb0820dc91
hdautovic1 <hdautovic1@etf.unsa.ba>
2020-10-26 18:32:22 +0100

    Dodan unos proizvoljnog broja.

--- src/ba/unsa/etf/rpr/tutorijal1/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal1/Main.java
@@ -1,11 +1,13 @@
 package ba.unsa.etf.rpr.tutorijal1;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite proizvoljan broj: ");
+        int n = ulaz.nextInt();
 
     }
-
     public static int sumaCifara(int n) {
         int suma=0;
      while(n != 0){
